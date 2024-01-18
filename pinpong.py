54098bbf516b0e7050663e5e842b1530b814b792
ashraf2113 <156348655+ashraf2113@users.noreply.github.com>
2024-01-18 11:54:23 +0200

    Update pinpong.py

--- pinpong.py
+++ pinpong.py
@@ -1,4 +1,5 @@
 import turtle
+import time
 
 wind = turtle.Screen()
 wind.title("ping pung")
@@ -34,33 +35,37 @@ ball.dy = 0.3
 score1 = 0
 score2 = 0
 
-score = turtle.Turtle()
-score.color("white")
-score.penup()
-score.hideturtle()
-score.goto(0, 260)
-score.write("player 1: 0 player 2: 0 ", align="center", font=("courier", 24, "normal"))
+score_display = turtle.Turtle()
+score_display.color("white")
+score_display.penup()
+score_display.hideturtle()
+score_display.goto(0, 260)
+score_display.write("player 1: 0 player 2: 0 ", align="center", font=("courier", 24, "normal"))
 
 # functions:
 def madrab1_up():
     y = madrab1.ycor()
     y += 20
-    madrab1.sety(y)
+    if y < 290:
+        madrab1.sety(y)
 
 def madrab1_down():
     y = madrab1.ycor()
     y -= 20
-    madrab1.sety(y)
+    if y > -290:
+        madrab1.sety(y)
 
 def madrab2_up():
     y = madrab2.ycor()
     y += 20
-    madrab2.sety(y)
+    if y < 290:
+        madrab2.sety(y)
 
 def madrab2_down():
     y = madrab2.ycor()
     y -= 20
-    madrab2.sety(y)
+    if y > -290:
+        madrab2.sety(y)
 
 wind.listen()
 wind.onkeypress(madrab1_up, "w")
@@ -69,36 +74,41 @@ wind.onkeypress(madrab2_up, "Up")
 wind.onkeypress(madrab2_down, "Down")
 
 while True:
+    wind.update()  # Update the graphics window
+
+    # Check for events and move paddles
     wind.update()
+    madrab1_up()
+    madrab1_down()
+    madrab2_up()
+    madrab2_down()
+
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-    if ball.ycor() > 290:
-        ball.sety(290)
-        ball.dy *= -1
-
-    if ball.ycor() < -290:
-        ball.sety(-290)
+    if ball.ycor() > 290 or ball.ycor() < -290:
         ball.dy *= -1
 
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score1 += 1
+        score_display.clear()
+        score_display.write("player 1: {} player 2: {}".format(score1, score2), align="center", font=("courier", 24, "normal"))
 
-    if ball.xcor() < -390:
+    elif ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
-
-    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < madrab2.ycor() + 40 and ball.ycor() > madrab2.ycor() - 40):
-        ball.setx(340)
-        ball.dx *= -1
-        score1 += 1
-        score.clear()
-        score.write("player 1: {} player 2: {}".format(score1, score2), align="center", font=("courier", 24, "normal"))
-
-    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < madrab1.ycor() + 40 and ball.ycor() > madrab1.ycor() - 40):
-        ball.setx(-340)
-        ball.dx *= -1
         score2 += 1
-        score.clear()
-        score.write("player 1: {} player 2: {}".format(score1, score2), align="center", font=("courier", 24, "normal"))
+        score_display.clear()
+        score_display.write("player 1: {} player 2: {}".format(score1, score2), align="center", font=("courier", 24, "normal"))
+
+    if (340 > ball.xcor() > 330) and (madrab2.ycor() + 40 > ball.ycor() > madrab2.ycor() - 40):
+        ball.setx(330)
+        ball.dx *= -1
+
+    elif (-340 < ball.xcor() < -330) and (madrab1.ycor() + 40 > ball.ycor() > madrab1.ycor() - 40):
+        ball.setx(-330)
+        ball.dx *= -1
+
+    time.sleep(0.01) 
